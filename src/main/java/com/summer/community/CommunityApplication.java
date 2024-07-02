184d16a5bc5a625a0afaf8322b86ea8896813112
gonghao <3075284085@qq.com>
2024-07-02 17:57:37 +0800

    0702

--- src/main/java/com/summer/community/CommunityApplication.java
+++ src/main/java/com/summer/community/CommunityApplication.java
@@ -1,11 +1,10 @@
 package com.summer.community;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 @MapperScan("com.summer.community.mapper")
 public class CommunityApplication {
 
