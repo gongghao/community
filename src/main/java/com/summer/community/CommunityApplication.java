da477251557d809eaa1153a82b1d8150ee22e669
gonghao <3075284085@qq.com>
2024-07-19 20:10:36 +0800

    Elasticsearch配置

--- src/main/java/com/summer/community/CommunityApplication.java
+++ src/main/java/com/summer/community/CommunityApplication.java
@@ -4,10 +4,17 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication()
 @MapperScan("com.summer.community.mapper")
 public class CommunityApplication {
 
+	@PostConstruct
+	public void init(){
+		// 解决netty启动冲突问题
+		System.setProperty("es.set.netty.runtime.available.processors", "false");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
 	}
