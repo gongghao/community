0caa4586b91bbda7e3b7c7e8eb373de57522c981
gonghao <3075284085@qq.com>
2024-08-16 19:54:31 +0800

    数据对接-DataController

--- src/main/java/com/summer/community/controller/DataController.java
+++ src/main/java/com/summer/community/controller/DataController.java
@@ -1,5 +1,6 @@
 package com.summer.community.controller;
 
+import com.summer.community.entity.Result;
 import com.summer.community.entity.User;
 import com.summer.community.service.DataService;
 import com.summer.community.util.CommunityUtil;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 
@@ -29,29 +31,40 @@ public class DataController {
     }
 
     @RequestMapping(path = "/data/uv", method = RequestMethod.POST)
+    @ResponseBody
     public String getUV(@DateTimeFormat(pattern = "yyyy-MM-dd") Date start,
                         @DateTimeFormat(pattern = "yyyy-MM-dd") Date end, Model model) {
+        Result result = Result.ok("/data/uv.post");
         long uv = dataService.calculateUV(start, end);
-        model.addAttribute("uvResult", uv);
+  //      model.addAttribute("uvResult", uv);
 //        System.out.println(start);
 //        System.out.println(end);
 //        for (int i = 0; i < 10; i++)
 //            System.out.println("");
-        model.addAttribute("uvStartDate", start);
-        model.addAttribute("uvEndDate", end);
-        return "forward:/data";
+     //   model.addAttribute("uvStartDate", start);
+       // model.addAttribute("uvEndDate", end);
+        result.data("uvResult", uv);
+        result.data("uvStartDate", start);
+        result.data("uvEndDate", end);
+     //   return "forward:/data";
+        return result.toString();
     }
 
     @RequestMapping(path = "/data/dau", method = RequestMethod.POST)
+    @ResponseBody
     public String getDAU(@DateTimeFormat(pattern = "yyyy-MM-dd") Date start,
                          @DateTimeFormat(pattern = "yyyy-MM-dd") Date end, Model model
     ) {
+        Result result = Result.ok("/data/dau.post");
         long dau = dataService.calculateDAU(start, end);
-        model.addAttribute("dauResult", dau);
-        model.addAttribute("dauStartDate", start);
-        model.addAttribute("dauEndDate", end);
-        return "forward:/data";
-
+//        model.addAttribute("dauResult", dau);
+//        model.addAttribute("dauStartDate", start);
+//        model.addAttribute("dauEndDate", end);
+//        return "forward:/data";
+        result.data("dauResult", dau);
+        result.data("dauStartDate", start);
+        result.data("dauEndDate", end);
+        return result.toString();
     }
 
 }
