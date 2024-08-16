79755ec66c8e969206804a5b4dcd67949509e88e
gonghao <3075284085@qq.com>
2024-08-16 20:25:03 +0800

    数据对接-DiscussPostController

--- src/main/java/com/summer/community/controller/DiscussPostController.java
+++ src/main/java/com/summer/community/controller/DiscussPostController.java
@@ -1,5 +1,6 @@
 package com.summer.community.controller;
 
+import com.baomidou.mybatisplus.extension.api.R;
 import com.summer.community.entity.*;
 import com.summer.community.event.EventProducer;
 import com.summer.community.service.CommentService;
@@ -56,10 +57,15 @@ public class DiscussPostController {
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
     public String addDiscussPost(String title, String content) {
+        Result result = Result.ok("/add.post");
         User user = hostHolder.getUser();
-        if (user == null)
-            return CommunityUtil.getJSONString(403, "用户未登录");
-
+        if (user == null) {
+           // return CommunityUtil.getJSONString(403, "用户未登录");
+            result.setSuccess(false);
+            result.setCode(403);
+            result.setMessage("用户未登录");
+            return result.toString();
+        }
         DiscussPost post = new DiscussPost();
         post.setUserId(user.getId());
         post.setTitle(title);
@@ -78,25 +84,35 @@ public class DiscussPostController {
         redisTemplate.opsForSet().add(redisKey, post.getId());
 
         // 报错的情况，将来统一处理
-        return CommunityUtil.getJSONString(0, "发布成功!");
+        result.setSuccess(true);
+        result.setCode(0);
+        result.setMessage("发布成功!");
+      //  return CommunityUtil.getJSONString(0, "发布成功!");
+        return result.toString();
     }
 
     @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
-    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Page page, Model model) {
+    @ResponseBody
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        Result result = Result.ok("/detail/{discussPostId}.get");
+        Page page = new Page();
         // 帖子
         DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
-        model.addAttribute("post", discussPost);
+        //model.addAttribute("post", discussPost);
+        result.data("post", discussPost);
         // 作者
         User user = userService.findUserById(discussPost.getUserId());
-        model.addAttribute("user", user);
+        //model.addAttribute("user", user);
+        result.data("user", user);
         // 点赞
         long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, discussPostId);
-        model.addAttribute("likeCount", likeCount);
+        //model.addAttribute("likeCount", likeCount);
+        result.data("likeCount", likeCount);
         // 点赞状态
         int likeStatus = hostHolder.getUser() == null ? 0 :
                 likeService.findEntityLikeStatus(hostHolder.getUser().getId(), ENTITY_TYPE_POST, discussPostId);
-        model.addAttribute("likeStatus", likeStatus);
-
+        //model.addAttribute("likeStatus", likeStatus);
+        result.data("likeStatus", likeStatus);
         // 评论分页信息
         page.setLimit(5);
         page.setPath("/discuss/detail/" + discussPostId);
@@ -150,25 +166,39 @@ public class DiscussPostController {
                 commentVoList.add(commentVo);
             }
 
-        model.addAttribute("comments", commentVoList);
+        //model.addAttribute("comments", commentVoList);
+        result.data("comments", commentVoList);
 
-        return "/site/discuss-detail";
+        result.data("Page", page);
+        return result.toString();
     }
 
     // 置顶
     @RequestMapping(path = "/top", method = RequestMethod.POST)
     @ResponseBody
     public String setTop(int id) {
+        Result result = Result.ok("/top.post");
+
         User user = hostHolder.getUser();
 //        System.out.println(user.getUsername());
 //        System.out.println(user.getType());
 //        for(int i = 0; i< 10; i++)
 //            System.out.println("");
 
-        if (user == null)
-            return CommunityUtil.getJSONString(403, "用户未登录");
-        else if (user.getType() != 2)
-            return CommunityUtil.getJSONString(401, "权限不足");
+        if (user == null) {
+           // return CommunityUtil.getJSONString(403, "用户未登录");
+            result.setSuccess(false);
+            result.setCode(403);
+            result.setMessage("用户未登录");
+            return result.toString();
+        }
+        else if (user.getType() != 2) {
+            //return CommunityUtil.getJSONString(401, "权限不足");
+            result.setSuccess(false);
+            result.setCode(401);
+            result.setMessage("权限不足");
+            return result.toString();
+        }
 
         Event event = new Event()
                 .setTopic(TOPIC_PUBLISH)
@@ -178,17 +208,34 @@ public class DiscussPostController {
         eventProducer.fireEvent(event);
         discussPostService.updateType(id, 1);
 
-        return CommunityUtil.getJSONString(0);
+        result.setSuccess(true);
+        result.setCode(0);
+        result.setMessage("成功");
+        return result.toString();
+       // return CommunityUtil.getJSONString(0);
     }
 
     @RequestMapping(path = "/wonderful", method = RequestMethod.POST)
     @ResponseBody
     public String setWonderful(int id) {
+        Result result = Result.ok("/wonderful.post");
+
         User user = hostHolder.getUser();
-        if (user == null)
-            return CommunityUtil.getJSONString(403, "用户未登录");
-        else if (user.getType() != 2)
-            return CommunityUtil.getJSONString(401, "权限不足");
+
+        if (user == null) {
+            // return CommunityUtil.getJSONString(403, "用户未登录");
+            result.setSuccess(false);
+            result.setCode(403);
+            result.setMessage("用户未登录");
+            return result.toString();
+        }
+        else if (user.getType() != 2) {
+            //return CommunityUtil.getJSONString(401, "权限不足");
+            result.setSuccess(false);
+            result.setCode(401);
+            result.setMessage("权限不足");
+            return result.toString();
+        }
 
         Event event = new Event()
                 .setTopic(TOPIC_PUBLISH)
@@ -201,17 +248,34 @@ public class DiscussPostController {
         String redisKey = RedisKeyUtil.getPostScoreKey();
         redisTemplate.opsForSet().add(redisKey, id);
 
-        return CommunityUtil.getJSONString(0);
+        //return CommunityUtil.getJSONString(0);
+        result.setSuccess(true);
+        result.setCode(0);
+        result.setMessage("成功");
+        return result.toString();
     }
 
     @RequestMapping(path = "/delete", method = RequestMethod.POST)
     @ResponseBody
     public String setDelete(int id) {
+        Result result = Result.ok("/delete.post");
+
         User user = hostHolder.getUser();
-        if (user == null)
-            return CommunityUtil.getJSONString(403, "用户未登录");
-        else if (user.getType() != 1)
-            return CommunityUtil.getJSONString(401, "权限不足");
+
+        if (user == null) {
+            // return CommunityUtil.getJSONString(403, "用户未登录");
+            result.setSuccess(false);
+            result.setCode(403);
+            result.setMessage("用户未登录");
+            return result.toString();
+        }
+        else if (user.getType() != 1) {
+            //return CommunityUtil.getJSONString(401, "权限不足");
+            result.setSuccess(false);
+            result.setCode(401);
+            result.setMessage("权限不足");
+            return result.toString();
+        }
 
         Event event = new Event()
                 .setTopic(TOPIC_DELETE)
@@ -221,6 +285,10 @@ public class DiscussPostController {
         eventProducer.fireEvent(event);
         discussPostService.updateStatus(id, 2);
 
-        return CommunityUtil.getJSONString(0);
+        //return CommunityUtil.getJSONString(0);
+        result.setSuccess(true);
+        result.setCode(0);
+        result.setMessage("成功");
+        return result.toString();
     }
 }
