7e3fc4c9fe26feb01b61b79b2851925eff36dbba
gonghao <3075284085@qq.com>
2024-08-28 08:30:01 +0800

    8.27随机匹配连接

--- src/main/java/com/summer/community/server/WebSocketServer.java
+++ src/main/java/com/summer/community/server/WebSocketServer.java
@@ -1,16 +1,20 @@
-package com.summer.community.server;
-
-
-import javax.websocket.*;
-import javax.websocket.server.PathParam;
-import javax.websocket.server.ServerEndpoint;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
+package com.example.demo;
 
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.concurrent.ConcurrentHashMap;
+
+import javax.websocket.server.PathParam;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @ServerEndpoint("/msgServer/{userId}")
 @Component
@@ -25,6 +29,10 @@ public class WebSocketServer {
      * concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
      */
     private static final ConcurrentHashMap<String, Session> webSocketMap = new ConcurrentHashMap<>();
+
+    private static final ConcurrentHashMap<String, Session> MatchPool = new ConcurrentHashMap<>();
+
+    private static final ConcurrentHashMap<String, String> MatchRelationship = new ConcurrentHashMap<>();
     /**
      * 与某个客户端的连接会话，需要通过它来给客户端发送数据
      */
@@ -44,6 +52,7 @@ public class WebSocketServer {
          */
         System.out.println(userId + " - 连接建立成功...");
         webSocketMap.put(userId, session);
+        //MatchPool.put(userId, session);
 
     }
 
@@ -73,27 +82,61 @@ public class WebSocketServer {
     public void sendMessage(String message) throws IOException {
         if (message.equals("心跳")){
             this.session.getBasicRemote().sendText(message);
-        }
-        Enumeration<String> keys = webSocketMap.keys();
-        while (keys.hasMoreElements()){
-            String key = keys.nextElement();
-            if (key.equals(this.userId)){
-                System.err.println("my id " + key);
-                continue;
+        } else
+        if (message.equals("准备连接")) {
+            System.out.println(this.userId + message);
+            if (!MatchPool.containsKey(this.userId) && !MatchRelationship.containsKey(this.userId))
+                MatchPool.put(this.userId, this.session);
+        } else
+        if (message.equals("退出连接")) {
+            System.out.println(this.userId + message);
+            if (MatchPool.containsKey(this.userId))
+                MatchPool.remove(this.userId);
+            else {
+                String target = MatchRelationship.get(this.userId);
+                MatchRelationship.remove(this.userId);
+                MatchRelationship.remove(target);
             }
-            if (webSocketMap.get(key) == null){
-                webSocketMap.remove(key);
-                System.err.println(key + " : null");
-                continue;
-            }
+        } else if (MatchRelationship.containsKey(this.userId)) {
+            //已连接
+            String key = MatchRelationship.get(this.userId);
             Session sessionValue = webSocketMap.get(key);
-            if (sessionValue.isOpen()){
-                System.out.println("发消息给: " + key + " ,message: " + message);
+            if (sessionValue.isOpen()) {
+                System.out.println("发消息给对方: " + key + " ,message: " + message);
                 sessionValue.getBasicRemote().sendText(message);
-            }else {
+            } else {
                 System.err.println(key + ": not open");
                 sessionValue.close();
                 webSocketMap.remove(key);
+            }
+        } else {//未连接
+            Enumeration<String> keys = MatchPool.keys();
+            while (keys.hasMoreElements()) {
+                String key = keys.nextElement();
+                if (key.equals(this.userId)) {
+                    System.err.println("my id " + key);
+                    continue;
+                }
+                if (webSocketMap.get(key) == null) {
+                    webSocketMap.remove(key);
+                    System.err.println(key + " : null");
+                    continue;
+                }
+                Session sessionValue = webSocketMap.get(key);
+                if (sessionValue.isOpen()) {
+                    System.out.println("群发消息给: " + key + " ,message: " + message);
+                    MatchPool.remove(this.userId);
+                    MatchPool.remove(key);
+                    MatchRelationship.put(this.userId, key);
+                    MatchRelationship.put(key, this.userId);
+                    System.out.println(this.userId + "  " + key + "已连接");
+                    sessionValue.getBasicRemote().sendText(message);
+                    break;
+                } else {
+                    System.err.println(key + ": not open");
+                    sessionValue.close();
+                    webSocketMap.remove(key);
+                }
             }
         }
     }
